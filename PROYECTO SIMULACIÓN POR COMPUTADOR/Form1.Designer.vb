f810ffad0c198b26071d02ea36b460d8ccdbdaad
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-08-04 23:02:20 -0500

    First change for numeral one

--- PROYECTO SIMULACIÓN POR COMPUTADOR/Form1.Designer.vb
+++ PROYECTO SIMULACIÓN POR COMPUTADOR/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,222 +20,133 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.TabPage3 = New System.Windows.Forms.TabPage()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
-        Me.TabPage2.SuspendLayout()
-        Me.TabPage3.SuspendLayout()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Piso = New System.Windows.Forms.PictureBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.xCirculo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.yCirculo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Piso, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveBorder
         Me.Label1.Font = New System.Drawing.Font("Maiandra GD", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(366, 52)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Menu
+        Me.Label1.Location = New System.Drawing.Point(361, 9)
         Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(695, 36)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "PROYECTO SIMULACIÓN POR COMPUTADOR"
         '
-        'Label2
+        'PictureBox1
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(97, 169)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(291, 22)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Ingresar el ángulo de lanzamiento"
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 48)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1044, 565)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
         '
-        'TextBox1
+        'Timer1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(437, 161)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(5)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(133, 30)
-        Me.TextBox1.TabIndex = 2
         '
-        'TextBox2
+        'Piso
         '
-        Me.TextBox2.Location = New System.Drawing.Point(437, 211)
-        Me.TextBox2.Margin = New System.Windows.Forms.Padding(5)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(133, 30)
-        Me.TextBox2.TabIndex = 3
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(647, 180)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(5)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(136, 45)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Calcular"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Piso.BackgroundImage = CType(resources.GetObject("Piso.BackgroundImage"), System.Drawing.Image)
+        Me.Piso.Location = New System.Drawing.Point(12, 613)
+        Me.Piso.Name = "Piso"
+        Me.Piso.Size = New System.Drawing.Size(1044, 39)
+        Me.Piso.TabIndex = 9
+        Me.Piso.TabStop = False
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(858, 116)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(5)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.xCirculo, Me.yCirculo})
+        Me.DataGridView1.Location = New System.Drawing.Point(1083, 48)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 51
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(595, 194)
-        Me.DataGridView1.TabIndex = 5
+        Me.DataGridView1.Size = New System.Drawing.Size(244, 296)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(1096, 396)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(203, 22)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Cantidad intersecciones"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(97, 219)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(1188, 430)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(235, 22)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Ingresar la velocidad inicial"
+        Me.Label3.Size = New System.Drawing.Size(21, 22)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "0"
         '
-        'TabControl1
+        'xCirculo
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Controls.Add(Me.TabPage3)
-        Me.TabControl1.Location = New System.Drawing.Point(214, 366)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(961, 344)
-        Me.TabControl1.TabIndex = 8
+        Me.xCirculo.HeaderText = "Angulo"
+        Me.xCirculo.Name = "xCirculo"
         '
-        'TabPage1
+        'yCirculo
         '
-        Me.TabPage1.Controls.Add(Me.PictureBox3)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 30)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(953, 310)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Controls.Add(Me.PictureBox2)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 30)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(953, 310)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'TabPage3
-        '
-        Me.TabPage3.Controls.Add(Me.PictureBox1)
-        Me.TabPage3.Location = New System.Drawing.Point(4, 30)
-        Me.TabPage3.Name = "TabPage3"
-        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage3.Size = New System.Drawing.Size(953, 310)
-        Me.TabPage3.TabIndex = 2
-        Me.TabPage3.Text = "TabPage3"
-        Me.TabPage3.UseVisualStyleBackColor = True
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(78, 66)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(5)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(10, 10)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 7
-        Me.PictureBox1.TabStop = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(471, 150)
-        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(5)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(10, 10)
-        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox2.TabIndex = 8
-        Me.PictureBox2.TabStop = False
-        '
-        'PictureBox3
-        '
-        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(471, 150)
-        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(5)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(10, 10)
-        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox3.TabIndex = 8
-        Me.PictureBox3.TabStop = False
+        Me.yCirculo.HeaderText = "Velocidad"
+        Me.yCirculo.Name = "yCirculo"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.GrayText
-        Me.ClientSize = New System.Drawing.Size(1540, 845)
-        Me.Controls.Add(Me.TabControl1)
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1370, 749)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Piso)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Font = New System.Drawing.Font("Maiandra GD", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaption
         Me.Margin = New System.Windows.Forms.Padding(5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
-        Me.TabPage2.ResumeLayout(False)
-        Me.TabPage3.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Piso, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Label3 As Label
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents TabPage3 As TabPage
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents PictureBox3 As PictureBox
-    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Piso As PictureBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents xCirculo As DataGridViewTextBoxColumn
+    Friend WithEvents yCirculo As DataGridViewTextBoxColumn
 End Class
--- PROYECTO SIMULACIÓN POR COMPUTADOR/Form1.vb
+++ PROYECTO SIMULACIÓN POR COMPUTADOR/Form1.vb
@@ -1,7 +1,107 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form2.Show()
+    Dim v0x, v0y, t As Double
+    Dim g As Double = 9.81
+    Dim velocidadInicial As Double = 50
+    Dim x, y As Double
+    Dim gBmp As Graphics
+    Dim bmp As Bitmap
+    Dim firstPoint As Boolean = True
+    Dim anguloActual As Double = 30 ' Ángulo inicial
+    Dim prevX, prevY As Double ' Coordenadas del punto anterior
+    Dim circleX, circleY As Integer ' Coordenadas del círculo
 
 
+
+    Dim cantidadInterseccion As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InicializarSimulacion()
+        circleX = New Random().Next(200, 501) ' Entre 200 y 500
+        circleY = New Random().Next(100, 301) ' Entre 100 y 300
     End Sub
+
+    Private Sub InicializarSimulacion()
+        Timer1.Interval = 1 ' Intervalo de actualización en milisegundos
+        Timer1.Start() 'Iniciar el timer      
+        ' Crear un bitmap para dibujar la trayectoria
+        bmp = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        gBmp = Graphics.FromImage(bmp)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        v0x = velocidadInicial * Math.Cos(anguloActual * Math.PI / 180) ' Formula para la velocidad inicial en X
+        v0y = velocidadInicial * Math.Sin(anguloActual * Math.PI / 180) ' Formula para la velocidad inicial en Y
+        t += 0.05 ' Incrementar el tiempo
+
+        ' Calcular nuevas coordenadas x e y
+        x = v0x * t
+        y = PictureBox1.Height - (v0y * t - 0.5 * g * t ^ 2) ' Ajustar la coordenada Y para que inicie en la parte de abajo del PICTUREBOX
+
+
+        Dim tiempoMaximo As Double = v0y / g ' Formula para encontrar el tiempo máximo
+        Dim distanciaMaximaX As Double = (Math.Pow(velocidadInicial, 2) * Math.Sin((anguloActual * 2) * Math.PI / 180)) / g ' Formula para saber la distancia maxima que se puede recorrer
+
+        ' Condicional para saber si se interseca con el circulo, sabiendo que, se debe poner un limite de 10 pixeles, por el tamaño del circulo
+        If (((Math.Round(x) - circleX) <= 10 And (Math.Round(x) - circleX) >= 0) And (Math.Round(y) - circleY) <= 10 And (Math.Round(y) - circleY) >= 0) Then
+            ' Validar que el valor del angulo y la velocidad no existan en la tabla
+            Dim filaExistente As DataGridViewRow = BuscarFilaExistente(anguloActual, velocidadInicial)
+            If filaExistente Is Nothing Then
+                ' Agregar ek valor a la tabla
+                DataGridView1.Rows.Add(anguloActual, velocidadInicial)
+                ' Aumentar la cantidad de intersecciones
+                cantidadInterseccion += 1
+            End If
+        End If
+
+        If x >= distanciaMaximaX OrElse anguloActual > 61 Then
+            PictureBox1.Refresh()
+            Console.WriteLine("Angulo: " & anguloActual & " velocidad: " & velocidadInicial)
+            If anguloActual > 60 And velocidadInicial >= 100 Then
+                Console.WriteLine("Hola")
+                Label3.Text = cantidadInterseccion
+                Form2.Show()
+                Timer1.Enabled = False
+            Else
+                If anguloActual > 60 And velocidadInicial < 100 Then
+                    anguloActual = 30
+                    velocidadInicial += 1
+                End If
+                ' Incrementar el ángulo actual y reiniciar la simulación
+                anguloActual += 1
+                prevX = -1 ' Reiniciar la coordenada X anterior
+                prevY = -1 ' Reiniciar la coordenada Y anterior
+                ReiniciarSimulacion()
+            End If
+        Else
+            ' Dibujar una línea desde el punto anterior al actual
+            If prevX <> -1 AndAlso prevY <> -1 Then
+                gBmp.DrawLine(Pens.Black, CInt(prevX), CInt(prevY), CInt(x), CInt(y))
+            End If
+            prevX = x ' Almacenar la coordenada X actual como previa
+            prevY = y ' Almacenar la coordenada Y actual como previa
+            PictureBox1.Refresh()
+        End If
+    End Sub
+
+    Private Sub ReiniciarSimulacion()
+        t = 0 ' Reiniciar el tiempo
+        x = 0 ' Reiniciar las coordenadas
+        y = 0
+        gBmp.Clear(Color.Transparent) ' Limpiar el gráfico actual, pero mantén la gráfica existente
+        Timer1.Start()
+    End Sub
+
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+        ' Dibujar el circulo en la posicion aleatoria
+        e.Graphics.FillEllipse(Brushes.Black, circleX, circleY, 10, 10)
+    End Sub
+
+    Private Function BuscarFilaExistente(angulo As Double, velocidad As Double) As DataGridViewRow
+        For Each fila As DataGridViewRow In DataGridView1.Rows
+            If fila.Cells("xCirculo").Value = anguloActual AndAlso fila.Cells("yCirculo").Value = velocidadInicial Then
+                Return fila
+            End If
+        Next
+        Return Nothing
+    End Function
 End Class
